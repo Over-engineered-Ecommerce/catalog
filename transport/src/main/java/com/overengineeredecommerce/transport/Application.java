dca270b133a7df32ead1255d212acd37ac893b65
GoncalvesF2 <filipe.goncalves@ppb.com>
2025-02-24 12:37:01 +0000

    fixed the entity and repository scan

--- transport/src/main/java/com/overengineeredecommerce/transport/Application.java
+++ transport/src/main/java/com/overengineeredecommerce/transport/Application.java
@@ -2,8 +2,15 @@ package com.overengineeredecommerce.transport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.overengineeredecommerce.transport",
+        "com.overengineeredecommerce.domain"
+})
+@EntityScan(basePackages = "com.overengineeredecommerce.domain.entity")
+@EnableJpaRepositories(basePackages = "com.overengineeredecommerce.transport.repository")
 public class Application {
 
     public static void main(String[] args) {
