9062fb19ca9cce00a624cd360e60b44b801f380f
GoncalvesF2 <filipe.goncalves@ppb.com>
2025-02-27 10:26:09 +0000

    Fix sonarqube

--- integration-test/src/test/java/com/overengineeredecommerce/integrationtest/StepDefinitions.java
+++ integration-test/src/test/java/com/overengineeredecommerce/integrationtest/StepDefinitions.java
@@ -4,7 +4,6 @@ import com.overengineeredecommerce.integrationtest.setup.Postgres;
 import com.overengineeredecommerce.transport.Application;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
@@ -35,19 +34,16 @@ public class StepDefinitions extends Postgres {
 
     @Given("a valid request to retrieve all categories")
     public void aValidRequestToRetrieveAllCategories() {
-        ValidatableResponse response = RestAssured.get(baseUrl + "/categories")
+        response = RestAssured.get(baseUrl + "/categories")
                 .then()
                 .statusCode(HttpStatus.OK.value())
                 .contentType(ContentType.JSON);
     }
 
-    @When("the request is sent")
-    public void theRequestIsSent() {
-    }
 
     @Then("the response status should be {int} OK")
-    public void theResponseStatusShouldBeOK(int StatusCode) {
-        response.statusCode(equalTo(StatusCode));
+    public void theResponseStatusShouldBeOK(int statusCode) {
+        response.statusCode(equalTo(statusCode));
 
     }
 }
